58c206957efc0ae0f73e8e19135d4f26fe770ca8
MehreenZahra <codewithMehreen1@gmail.com>
2026-02-01 00:12:24 +0500

    Add initial blog models and migration files, including custom manager for published posts and ordering by published date.

--- blog/models.py
+++ blog/models.py
@@ -11,6 +11,10 @@ class Category(models.Model):
         return self.name
 
 class Post(models.Model):
+
+    class PostObjects(models.Manager):
+        def get_queryset(self):
+            return super().get_queryset().filter(status='published')
     options = (
         ('draft', 'Draft'),
         ('published', 'Published'),
@@ -23,3 +27,11 @@ class Post(models.Model):
     published = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     status = models.CharField(max_length=10, choices=options, default='published')
+    objects = models.Manager() # default manager
+    postobjects = PostObjects() # custom manager
+
+    class Meta:
+        ordering = ('-published',)
+
+    def __str__(self):
+        return self.title
